0d9ded59c047811d2dd2a00a77e798900143020a
albertKCR <kirchner_beto@hotmail.com>
2022-08-18 18:48:00 -0300

    matriz info com coordenadas e cores

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import pyautogui
 import PIL.ImageGrab
+import math
 
 dic = r'img2.png'
 img = cv.imread(dic)
-cv.imshow("x",img)
+#cv.imshow("x",img)
+
 
 cor_para_amarelo = (255, 0, 0)
 
 im = Image.open(dic).convert('RGB')
+#plt.imshow(im)
+#plt.show()
 
 # Pega a imagem como um numpy.array com formato altura x largura x num_canais
 data = np.array(im)
@@ -25,15 +29,15 @@ data[condicao.T] = cor_para_amarelo
 
 # Volto o array para uma imagem do PIL
 im2 = Image.fromarray(data)
-im2.save("aaa.png")
+im2.save("seeYellow.png")
 
 
-img = cv.imread("aaa.png")
-cv.imshow("y",img)
+img = cv.imread("seeYellow.png")
+#cv.imshow("y",img)
 
 grey = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
-plt.imshow(grey,cmap="gray")
-plt.show()
+#plt.imshow(grey,cmap="gray")
+#plt.show()
 
 
 kernel = np.ones((2,2),np.uint8)
@@ -41,15 +45,15 @@ kernel = np.ones((2,2),np.uint8)
 grey = cv.GaussianBlur(grey,(9,9),0)
 grey = cv.morphologyEx(grey, cv.MORPH_OPEN, kernel)
 grey = cv.morphologyEx(grey, cv.MORPH_CLOSE, kernel)
-plt.imshow(grey,cmap="gray")
-plt.show()
+#plt.imshow(grey,cmap="gray")
+#plt.show()
 
 canny = cv.Canny(grey,170,200)
-plt.imshow(canny,cmap="gray")
-plt.show()
+#plt.imshow(canny,cmap="gray")
+#plt.show()
 
 
-circles = circles = cv.HoughCircles(canny,
+circles = cv.HoughCircles(canny,
                           cv.HOUGH_GRADIENT,
                           dp=1.1,
                           minDist=5,
@@ -71,13 +75,13 @@ for i in circles[0,:]:
   print(i)
 
 
-img1 = cv.imread("img3.png")
+#img1 = cv.imread("img3.png")
 
 
 print("###")
 
 
-plt.imshow(cimg)
+#plt.imshow(cimg)
 
 red_image = PIL.Image.open(dic)
 red_image_rgb = red_image.convert("RGB")
@@ -85,7 +89,8 @@ counter = 0
 verde = 0
 vermelho = 0
 amarelo = 0
-print(circles[0,1])
+
+
 for i in circles[0,:]:
   rgb_pixel_value = red_image_rgb.getpixel((i[0],i[1]))
   print(rgb_pixel_value)
@@ -97,10 +102,43 @@ for i in circles[0,:]:
     amarelo = amarelo+1
   counter = 1+counter
 
+postes_livre = amarelo+verde
+
+#info[0,x]=coordenada, info[1,x]=cor
+info = []
+for y in range(2):
+  linha = []
+  for x in range(postes_livre):
+    linha.append(0)
+  info.append(linha)
+aux1 = 0
+for i in circles[0,:]:
+  rgb_pixel_value = red_image_rgb.getpixel((i[0],i[1]))
+  print(rgb_pixel_value)
+  if rgb_pixel_value == (38, 115, 0):
+    verde = verde+1
+  elif rgb_pixel_value == (255, 0, 0):
+    vermelho = vermelho+1
+  elif rgb_pixel_value == (255, 170, 0):
+    amarelo = amarelo+1
+  if (rgb_pixel_value == (255, 170, 0)) or (rgb_pixel_value == (38, 115, 0)):
+    info[0][aux1] = i
+    info[1][aux1] = rgb_pixel_value
+    aux1 = aux1+1
+  counter = 1+counter
+
 
 print("Número de postes:", counter)
 print("Número de postes em verde:", verde)
 print("Número de postes em vermelho:", vermelho)
 print("Número de postes em amarelo:", amarelo)
-postes_livre = amarelo+verde
 print("Postes livres (amarelo e verde):", postes_livre)
+
+plt.imshow(im)
+plt.plot([18,45],[42,54])
+plt.show()
+print(type(circles))
+print(math.dist([18,45],[42,54]))
+
+
+print(info)
